221af38f65b236d0a3522c244c513725872c87aa
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-12 10:26:19 -0500

    Initial commit

---
+++ Contents/mods/SOAE/media/lua/server/Items/Distributions_SOAEClothing.lua
@@ -0,0 +1,34 @@
+require 'Items/ProceduralDistributions'
+
+
+table.insert(ProceduralDistributions.list.CrateToys.items, "SOAEClothing.Hat_PirateHat");
+table.insert(ProceduralDistributions.list.CrateToys.items, 3);
+table.insert(ProceduralDistributions.list.GigamartToys.items, "SOAEClothing.Hat_PirateHat");
+table.insert(ProceduralDistributions.list.GigamartToys.items, 3);
+
+table.insert(ProceduralDistributions.list.CrateToys.items, "SOAEClothing.Hat_SkullMask");
+table.insert(ProceduralDistributions.list.CrateToys.items, 3);
+table.insert(ProceduralDistributions.list.GigamartToys.items, "SOAEClothing.Hat_SkullMask");
+table.insert(ProceduralDistributions.list.GigamartToys.items, 3);
+
+table.insert(ProceduralDistributions.list.CrateToys.items, "SOAEClothing.Hat_SkullMask");
+table.insert(ProceduralDistributions.list.CrateToys.items, 3);
+table.insert(ProceduralDistributions.list.GigamartToys.items, "SOAEClothing.Hat_SkullMask");
+table.insert(ProceduralDistributions.list.GigamartToys.items, 3);
+
+table.insert(ProceduralDistributions.list.ClothingStorageHeadwear.items, "SOAEClothing.Hat_DesperadoHat");
+table.insert(ProceduralDistributions.list.ClothingStorageHeadwear.items, 1);
+table.insert(ProceduralDistributions.list.WardrobeManClassy.items, "SOAEClothing.Hat_DesperadoHat");
+table.insert(ProceduralDistributions.list.WardrobeManClassy.items, 3);
+
+table.insert(ProceduralDistributions.list.CrateSports.items, "SOAEClothing.Shoe_Cleats");
+table.insert(ProceduralDistributions.list.CrateSports.items, 3);
+table.insert(ProceduralDistributions.list.SportStoreSneakers.items, "SOAEClothing.Shoe_Cleats");
+table.insert(ProceduralDistributions.list.SportStoreSneakers.items, 3);
+table.insert(ProceduralDistributions.list.SchoolLockers.items, "SOAEClothing.Shoe_Cleats");
+table.insert(ProceduralDistributions.list.SchoolLockers.items, 2);
+
+
+
+
+
