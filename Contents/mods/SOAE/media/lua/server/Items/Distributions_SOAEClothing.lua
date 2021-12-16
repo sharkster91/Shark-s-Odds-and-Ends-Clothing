3ef3c952040a6b8a9879977a237f6e10eb69b259
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-15 20:15:25 -0500

    Content update

--- Contents/mods/SOAE/media/lua/server/Items/Distributions_SOAEClothing.lua
+++ Contents/mods/SOAE/media/lua/server/Items/Distributions_SOAEClothing.lua
@@ -23,6 +23,14 @@ table.insert(ProceduralDistributions.list.ClothingStorageHeadwear.items, 1);
 table.insert(ProceduralDistributions.list.WardrobeManClassy.items, "SOAEClothing.Hat_DesperadoHat");
 table.insert(ProceduralDistributions.list.WardrobeManClassy.items, 3);
 
+table.insert(ProceduralDistributions.list.ArmySurplusOutfit.items, "SOAEClothing.BlackWebbing");
+table.insert(ProceduralDistributions.list.ArmySurplusOutfit.items, 3);
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, "SOAEClothing.BlackWebbing");
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, 3);
+table.insert(ProceduralDistributions.list.CampingStoreClothes.items, "SOAEClothing.BlackWebbing");
+table.insert(ProceduralDistributions.list.CampingStoreClothes.items, 2);
+
+
 table.insert(ProceduralDistributions.list.CrateSports.items, "SOAEClothing.Shoe_Cleats");
 table.insert(ProceduralDistributions.list.CrateSports.items, 3);
 table.insert(ProceduralDistributions.list.SportStoreSneakers.items, "SOAEClothing.Shoe_Cleats");
