f89f1568cd144d94ff9a028a3bb27ac553d18c48
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-26 18:52:58 -0500

    Tweaked spawns

--- Contents/mods/SOAE/media/lua/server/Items/Distributions_SOAEClothing.lua
+++ Contents/mods/SOAE/media/lua/server/Items/Distributions_SOAEClothing.lua
@@ -1,18 +1,18 @@
 require 'Items/ProceduralDistributions'
 
 table.insert(ProceduralDistributions.list.CrateToys.items, "SOAEClothing.Hat_PirateHat");
-table.insert(ProceduralDistributions.list.CrateToys.items, 3);
+table.insert(ProceduralDistributions.list.CrateToys.items, 1);
 table.insert(ProceduralDistributions.list.GigamartToys.items, "SOAEClothing.Hat_PirateHat");
-table.insert(ProceduralDistributions.list.GigamartToys.items, 3);
+table.insert(ProceduralDistributions.list.GigamartToys.items, 0.05);
 table.insert(ProceduralDistributions.list.StripClubDressers.items, "SOAEClothing.Hat_PirateHat");
-table.insert(ProceduralDistributions.list.StripClubDressers.items, 3);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 0.05);
 
 table.insert(ProceduralDistributions.list.CrateToys.items, "SOAEClothing.Glasses_HeartGlasses");
-table.insert(ProceduralDistributions.list.CrateToys.items, 3);
+table.insert(ProceduralDistributions.list.CrateToys.items, 1);
 table.insert(ProceduralDistributions.list.GigamartToys.items, "SOAEClothing.Glasses_HeartGlasses");
-table.insert(ProceduralDistributions.list.GigamartToys.items, 3);
+table.insert(ProceduralDistributions.list.GigamartToys.items, 0.05);
 table.insert(ProceduralDistributions.list.StripClubDressers.items, "SOAEClothing.Glasses_HeartGlasses");
-table.insert(ProceduralDistributions.list.StripClubDressers.items, 3);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 0.05);
 
 table.insert(ProceduralDistributions.list.StripClubDressers.items, "SOAEClothing.Hat_DungeonMaster");
 table.insert(ProceduralDistributions.list.StripClubDressers.items, 5);
@@ -20,14 +20,14 @@ table.insert(ProceduralDistributions.list.Locker.items, "SOAEClothing.Hat_Dungeo
 table.insert(ProceduralDistributions.list.Locker.items, 0.01);
 
 table.insert(ProceduralDistributions.list.CrateToys.items, "SOAEClothing.Hat_SkullMask");
-table.insert(ProceduralDistributions.list.CrateToys.items, 3);
+table.insert(ProceduralDistributions.list.CrateToys.items, 0.05);
 table.insert(ProceduralDistributions.list.GigamartToys.items, "SOAEClothing.Hat_SkullMask");
-table.insert(ProceduralDistributions.list.GigamartToys.items, 3);
+table.insert(ProceduralDistributions.list.GigamartToys.items, 0.05);
 
 table.insert(ProceduralDistributions.list.CrateToys.items, "SOAEClothing.Hat_WizardHat");
-table.insert(ProceduralDistributions.list.CrateToys.items, 3);
+table.insert(ProceduralDistributions.list.CrateToys.items, 0.05);
 table.insert(ProceduralDistributions.list.GigamartToys.items, "SOAEClothing.Hat_WizardHat");
-table.insert(ProceduralDistributions.list.GigamartToys.items, 3);
+table.insert(ProceduralDistributions.list.GigamartToys.items, 0.05);
 
 table.insert(ProceduralDistributions.list.CrateToys.items, "SOAEClothing.Hat_SkullMask");
 table.insert(ProceduralDistributions.list.CrateToys.items, 3);
@@ -35,13 +35,13 @@ table.insert(ProceduralDistributions.list.GigamartToys.items, "SOAEClothing.Hat_
 table.insert(ProceduralDistributions.list.GigamartToys.items, 3);
 
 table.insert(ProceduralDistributions.list.ClothingStorageHeadwear.items, "SOAEClothing.Hat_DesperadoHat");
-table.insert(ProceduralDistributions.list.ClothingStorageHeadwear.items, 1);
+table.insert(ProceduralDistributions.list.ClothingStorageHeadwear.items, 0.05);
 table.insert(ProceduralDistributions.list.WardrobeManClassy.items, "SOAEClothing.Hat_DesperadoHat");
-table.insert(ProceduralDistributions.list.WardrobeManClassy.items, 3);
+table.insert(ProceduralDistributions.list.WardrobeManClassy.items, 0.5);
 
 table.insert(ProceduralDistributions.list.CrateSports.items, "SOAEClothing.Shoe_Cleats");
-table.insert(ProceduralDistributions.list.CrateSports.items, 3);
+table.insert(ProceduralDistributions.list.CrateSports.items, 1);
 table.insert(ProceduralDistributions.list.SportStoreSneakers.items, "SOAEClothing.Shoe_Cleats");
-table.insert(ProceduralDistributions.list.SportStoreSneakers.items, 3);
+table.insert(ProceduralDistributions.list.SportStoreSneakers.items, 1);
 table.insert(ProceduralDistributions.list.SchoolLockers.items, "SOAEClothing.Shoe_Cleats");
-table.insert(ProceduralDistributions.list.SchoolLockers.items, 2); 
+table.insert(ProceduralDistributions.list.SchoolLockers.items, 1); 
